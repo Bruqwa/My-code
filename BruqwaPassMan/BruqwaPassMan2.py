cf40a22c4a50038b012c546bc01c1cd74882a65d
Egor Shabalin <0403440@gmail.com>
2021-08-10 20:12:01 +0300

    Update BruqwaPassMan2.py

--- BruqwaPassMan/BruqwaPassMan2.py
+++ BruqwaPassMan/BruqwaPassMan2.py
@@ -1,4 +1,5 @@
-from tkinter import *
+import tkinter
+from tkinter import ttk
 import random
 
 def generator(length):
@@ -27,7 +28,7 @@ def generator(length):
 	password.extend(random.choices(allchars, weights=None, cum_weights=None, k=saltlen))
 	random.shuffle(password)
 	password = ''.join(password)
-	out.delete(0, END)
+	out.delete(0, tkinter.END)
 	out.insert(0, password)
 
 def press_key(event):
@@ -36,36 +37,60 @@ def press_key(event):
 	else:
 		pass
 
-window = Tk()
+window = tkinter.Tk()
 
 window.bind('<Key>', press_key)
 
 window['bg'] = '#000000'
 window.title('BruqwaPassMan')
-#window.wm_attributes('-alpha', 0,7)
-window.geometry('230x270')
+
+window.geometry('770x400')
 window.resizable(width=True, height = True)
 
-canvas = Canvas(window, height = 300, width = 300)
+canvas = tkinter.Canvas(window, height = 300, width = 300)
 canvas.pack()
 
-frame = Frame(window, bg = 'black')
+frame = tkinter.Frame(window, bg = 'black')
 frame.place(relx = 0, rely = 0, relwidth = 1, relheight = 1)
 
-title1 = Label(frame, text = 'Enter password length\n(should be more then 4): ', bg = 'black', fg = 'orange', font = 40)
+title1 = tkinter.Label(frame, text = 'Enter password length\n(should be more then 4): ', bg = 'black', fg = 'orange', font =70)
 title1.grid(row = 0, stick = 'we', padx = 10, pady = 10)
 
-userInput = Entry(frame, bg = 'black', fg = 'orange', justify='center', insertbackground = 'orange')
+userInput = tkinter.Entry(frame, bg = 'black', fg = 'orange', justify='center', insertbackground = 'orange')
 userInput.grid(row = 1, stick = 'we', padx = 10, pady = 10)
 
-btn = Button(frame, text = 'Generate', bg = 'orange', command = lambda: generator(userInput.get()))
-btn.grid(row = 2, stick = 'we', padx = 10, pady = 10)
+btnGen = tkinter.Button(frame, text = 'Generate', bg = 'orange', command = lambda: generator(userInput.get()))
+btnGen.grid(row = 2, stick = 'we', padx = 10, pady = 10)
 
-title2 = Label(frame, text = 'Your password is: ', bg = 'black', fg = 'orange', font = 40)
+title2 = tkinter.Label(frame, text = 'Your password is: ', bg = 'black', fg = 'orange', font =70)
 title2.grid(row = 3, stick = 'we', padx = 10, pady = 10)
 
-out = Entry(frame, bg = 'black', fg = 'lime', justify='center', insertbackground = 'lime')
+out = tkinter.Entry(frame, bg = 'black', fg = 'lime', justify='center', insertbackground = 'lime')
 out.grid(row = 4, stick = 'we', padx = 10, pady = 10)
+
+title3 = tkinter.Label(frame, text = 'Enter name to save\ngenerated password: ', bg = 'black', fg = 'orange', font =70)
+title3.grid(column = 0, row = 5, stick = 'we', padx = 10, pady = 10)
+
+inputPassName = tkinter.Entry(frame, bg = 'black', fg = 'orange', justify='center', insertbackground = 'orange')
+inputPassName.grid(column = 0, row = 6, stick = 'we', padx = 10, pady = 10)
+
+btnSave = tkinter.Button(frame, text = 'Save password', bg = 'orange')
+btnSave.grid(column = 0, row = 7, stick = 'we', padx = 10, pady = 10)
+
+btnDelete = tkinter.Button(frame, text = 'Delete password', bg = 'orange')
+btnDelete.grid(column = 1, stick = 'w', row = 7, padx = 0, pady = 10)
+
+s = ttk.Style()
+s.configure('Treeview', background = '#000')
+
+tree = ttk.Treeview(frame, columns = ('ID', 'Name', 'Password'), height = 15, show = 'headings')
+tree.column('ID', width = 30)
+tree.column('Name', width = 250)
+tree.column('Password', width = 250)
+tree.heading('ID', text = 'ID')
+tree.heading('Name', text = 'Name')
+tree.heading('Password', text = 'Password')
+tree.grid(column = 1, row = 0, rowspan = 7)
 
 
 
