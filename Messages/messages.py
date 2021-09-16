9324051fb07f8b618619ae9fd01ee3d4026bf942
Egor Shabalin <0403440@gmail.com>
2021-09-16 15:42:51 +0300

    Update messages.py

--- Messages/messages.py
+++ Messages/messages.py
@@ -7,6 +7,12 @@ app = FastAPI()
 @app.websocket("/messages")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
+    item_number = 0
     while True:
+        item_number = item_number + 1
         data = await websocket.receive_text()
-        await websocket.send_text(data)
+        j = json.loads(data)
+        j['input'] = str(item_number) + '.' + ' '
+        j = json.dumps(j)
+        #print(j)
+        await websocket.send_text(j)
